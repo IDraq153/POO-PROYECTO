087dde3f4868ab3d959856cc4a281d3c1400cfcb
Dario <darioruamelendez@gmail.com>
2025-11-16 11:34:30 -0500

    OrdeneCode

--- src/Ventana/Menu.java
+++ src/Ventana/Menu.java
@@ -4,10 +4,9 @@ package Ventana;
 import java.awt.Color;
 import static java.lang.System.exit;
 public class Menu extends javax.swing.JFrame {
-    
+   
     int mousepX;
     int mousepY;
-    
     //CONSTRUCTOR
     public Menu() {
         initComponents();
@@ -431,13 +430,13 @@ public class Menu extends javax.swing.JFrame {
                 }
             }
         } catch (ClassNotFoundException ex) {
-            java.util.logging.Logger.getLogger(a.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(Menu.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         } catch (InstantiationException ex) {
-            java.util.logging.Logger.getLogger(a.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(Menu.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         } catch (IllegalAccessException ex) {
-            java.util.logging.Logger.getLogger(a.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(Menu.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         } catch (javax.swing.UnsupportedLookAndFeelException ex) {
-            java.util.logging.Logger.getLogger(a.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(Menu.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         }
         //</editor-fold>
 
